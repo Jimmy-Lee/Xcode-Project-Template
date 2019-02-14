a35cc1ea783e16dd658a0abc094d4b8dc48ec032
Jimmy Lee <jimmylee@synology.com>
2019-02-14 17:28:37 +0800

    add project template

---
+++ Prime.xctemplate/AppDelegate.swift
@@ -0,0 +1,17 @@
+//___FILEHEADER___
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = RootViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+}
---
+++ Prime.xctemplate/RootViewController.swift
@@ -0,0 +1,9 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
